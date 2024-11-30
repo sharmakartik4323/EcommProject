837fd2860e7f09867bd59d1e261f74c399ec0c55
sharmakartik4323 <161711021+sharmakartik4323@users.noreply.github.com>
2024-11-30 14:28:28 +0530

    Update ISP_CALL.cs

--- EcommProject.DataAccess/Repositoty/IRepository/ISP_CALL.cs
+++ EcommProject.DataAccess/Repositoty/IRepository/ISP_CALL.cs
@@ -9,10 +9,10 @@ namespace EcommProject.DataAccess.Repository.IRepository
 {
     public interface ISP_CALL:IDisposable
     {
-        void Execute(string procedureName, DynamicParameters param=null); //it works for Save,Update and Delete. Execute is name here we can name anything
-        T Single<T>(string procedureName, DynamicParameters param=null); //Single is name here we can write anything here
+        void Execute(string procedureName, DynamicParameters param=null); 
+        T Single<T>(string procedureName, DynamicParameters param=null); 
         IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null);
-        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);// Used to return result of more than 1 query
+        Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null);
         T OneRecord<T>(string procedureName, DynamicParameters param = null);
     }
 }
