d1748e7f6fdf30cb26cf0a6306be3ea25515c67a
sharmakartik4323 <161711021+sharmakartik4323@users.noreply.github.com>
2024-11-30 12:29:13 +0530

    Add files via upload

---
+++ EcommProject.Utility/ISMSService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace EcommProject.Utility
+{
+    public interface ISMSService
+    {
+        Task <MessageResource> SendAsync(string message, string to);
+        Task <CallResource> SendCallAsync(string message, string to);
+    }
+}
---
+++ EcommProject.Utility/SMSService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net.Mail;
+using System.Net;
+
+namespace EcommProject.Utility
+{
+    public class SMSService : ISMSService
+    {
+        private readonly SMSSettings _sMSSettings;
+        public SMSService(IOptions<SMSSettings> sMSSettings)
+        {
+            _sMSSettings = sMSSettings.Value;
+        }
+        public async Task<MessageResource> SendAsync(string to, string message)
+        {
+            TwilioClient.Init(_sMSSettings.AccountSID, _sMSSettings.AuthToken);
+
+           var result = await MessageResource.CreateAsync(
+                body: message,
+                from: new PhoneNumber(_sMSSettings.PhoneNumber),
+                to: new PhoneNumber(to)
+                );
+            return result;
+        }
+
+        public async Task<CallResource> SendCallAsync(string to, string message)
+        {
+            TwilioClient.Init(_sMSSettings.AccountSID, _sMSSettings.AuthToken);
+
+            var result = await CallResource.CreateAsync(
+                 from: new PhoneNumber(_sMSSettings.PhoneNumber),
+                 to: new PhoneNumber(to),
+                 twiml: new Twiml($"<Response><Say>{message}</Say></Response>")
+                 );
+            return result;
+        }
+    }
+}
