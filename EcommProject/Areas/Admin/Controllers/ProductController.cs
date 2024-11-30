21a8638ea28b16e1c0f11b4d8c9da4163edc32cb
sharmakartik4323 <161711021+sharmakartik4323@users.noreply.github.com>
2024-11-30 22:26:45 +0530

    Add files via upload

---
+++ EcommProject/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,146 @@
+﻿using EcommProject.DataAccess.Repository.IRepository;
+using EcommProject.Models;
+using EcommProject.Models.ViewModels;
+using EcommProject.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EcommProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin+","+SD.Role_Employee)]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region APIs
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Product.GetAll() });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var productInDb = _unitOfWork.Product.Get(id);
+            if (productInDb == null)
+                return Json(new { success = false, message = "Something Went Wrong!" });
+            //Image Delete
+            var webRootPath = _webHostEnvironment.WebRootPath;
+            var imagePath = Path.Combine(webRootPath, productInDb.ImageUrl.Trim('\\'));
+            if (System.IO.File.Exists(imagePath))
+            {
+                System.IO.File.Delete(imagePath);
+            }
+            //Data Delete
+            _unitOfWork.Product.Remove(productInDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Deleted Successfully" });
+        }
+        #endregion
+        public IActionResult Upsert(int? id)
+        {
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(cl => new SelectListItem()
+                {
+                    Text = cl.Name,
+                    Value = cl.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(cl => new SelectListItem()
+                {
+                    Text = cl.Name,
+                    Value = cl.Id.ToString()
+                })
+            };
+            if (id == null) return View(productVM); //Create
+            //Edit
+            productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            if (productVM.Product == null) return NotFound();
+            return View(productVM);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM productVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var webRootPath = _webHostEnvironment.WebRootPath; //WebRootPath apne aap path uthalega wwwroot tak kaa
+                var files = HttpContext.Request.Form.Files; //Form me jaa kr Image baale section tak jaaega
+                if (files.Count() > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString(); //This is used to generate file name randomly on server, because file name same nhi hona cahiye, otherwise file overwrite hojaegi.GUID = Global Uni Identifier
+                    var extension = Path.GetExtension(files[0].FileName); // Ek hi file kaa extension uthaega or usko hi extension bnaega, NOT Applicable in Case of Multiple files
+                    var uploads = Path.Combine(webRootPath, @"images\products");//Use to combine Path i.e. fileName+extension , webRootPath wwwroot tak utaega Path, @ for escape sequence
+                    if (productVM.Product.Id != 0)
+                    {
+                        var imageExists = _unitOfWork.Product.Get(productVM.Product.Id).ImageUrl;
+                        productVM.Product.ImageUrl = imageExists;
+                    }
+                    if (productVM.Product.ImageUrl != null)
+                    {
+                        var imagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.Trim('\\'));
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+                    }
+                    using (var fileStream = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create)) //uploads uper ek variable bnaya hai jisme file bnegi, FileMode.Create means File bnaegaa
+                    {
+                        files[0].CopyTo(fileStream); //file[0] me actual file hai jo browse krke select ki  thi CopyTo usko fileStream me write kr dega
+                    }
+                    productVM.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                }
+                else
+                {
+                    if (productVM.Product.Id != 0)
+                    {
+                        var imageExists = _unitOfWork.Product.Get(productVM.Product.Id).ImageUrl;
+                        productVM.Product.ImageUrl = imageExists;
+                    }
+                }
+                if (productVM.Product.Id == 0)
+                    _unitOfWork.Product.Add(productVM.Product);
+                else
+                    _unitOfWork.Product.Update(productVM.Product);
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                productVM = new ProductVM()
+                {
+                    Product = new Product(),
+                    CategoryList = _unitOfWork.Category.GetAll().Select(cl => new SelectListItem()
+                    {
+                        Text = cl.Name,
+                        Value = cl.Id.ToString()
+                    }),
+                    CoverTypeList = _unitOfWork.CoverType.GetAll().Select(cl => new SelectListItem()
+                    {
+                        Text = cl.Name,
+                        Value = cl.Id.ToString()
+                    })
+                };
+                if (productVM.Product.Id != 0)
+                {
+                    productVM.Product = _unitOfWork.Product.Get(productVM.Product.Id);
+                }
+                return View(productVM);
+            }
+        }
+    }
+}
---
+++ EcommProject/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,79 @@
+﻿using EcommProject.DataAccess.Data;
+using EcommProject.DataAccess.Repository.IRepository;
+using EcommProject.Models;
+using EcommProject.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ApplicationDbContext _context;
+        public UserController(IUnitOfWork unitOfWork, ApplicationDbContext context)
+        {
+            _unitOfWork = unitOfWork;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region APIs
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userList = _context.ApplicationUsers.ToList();//aspnetuser
+            var roles = _context.Roles.ToList();//aspnetroles
+            var userRoles = _context.UserRoles.ToList();//aspnetuserrole
+            foreach (var user in userList)
+            {
+                var roleId = userRoles.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(r => r.Id == roleId).Name;
+                if (user.CompanyId == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+                if (user.CompanyId != null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = _unitOfWork.Company.Get(Convert.ToInt32(user.CompanyId)).Name
+                    };
+                }
+            }
+            //Remove admin role user from List
+            var adminUser = userList.FirstOrDefault(u => u.Role == SD.Role_Admin);
+            userList.Remove(adminUser);
+            return Json(new { data = userList });
+        }
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)//[FromBody] means user.js se data aega, means form,body se Data aega
+        {
+            bool isLocked = false;
+            var userInDb = _context.ApplicationUsers.FirstOrDefault(au => au.Id == id);
+            if (userInDb == null)
+                return Json(new { success = false, message = "Something went wrong while lock and unlock user!!!" });
+            if(userInDb!=null&& userInDb.LockoutEnd>DateTime.Now)
+            {
+                userInDb.LockoutEnd = DateTime.Now;
+                isLocked = false;
+            }
+            else
+            {
+                userInDb.LockoutEnd = DateTime.Now.AddYears(100);
+                isLocked = true;
+            }
+            _context.SaveChanges();
+            return Json(new { success = true, message = isLocked == true?"User Locked Successfully":"User Unlocked Successfully" });
+        }
+        #endregion
+    }
+}
