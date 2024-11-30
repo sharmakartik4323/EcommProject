b7baef91e66271d613145d59995b5c0c5c986f65
sharmakartik4323 <161711021+sharmakartik4323@users.noreply.github.com>
2024-11-30 12:36:07 +0530

    Add files via upload

---
+++ EcommProject.Models/Address.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace EcommProject.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ EcommProject.Models/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommProject.Models
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+            Count = 1;
+        }
+        public int Id { get; set; }
+        public string ApplicationUserId { get; set; }//Column Name
+        [ForeignKey("ApplicationUserId")]//ApplicationUser Table ke Id Column se foreign key bnegi
+        public ApplicationUser ApplicationUser { get; set; }
+        public int  ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+        public int Count { get; set; }
+        [NotMapped]
+        public double Price { get; set; } //Price fix nhi hai isliye price ko NotMapped rkha
+        public bool IsSelected { get; set; }
+        public bool IsRemoveFromCart { get; set; }
+    }
+}
