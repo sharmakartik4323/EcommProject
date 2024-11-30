cef050b657739ead8646fe30478c79024ce320c0
sharmakartik4323 <161711021+sharmakartik4323@users.noreply.github.com>
2024-11-30 12:41:31 +0530

    Add files via upload

---
+++ EcommProject.DataAccess/20241021152316_AddColumnsToAddressModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommProject.DataAccess.Migrations
+{
+    public partial class AddColumnsToAddressModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Addresses",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_ApplicationUserId",
+                table: "Addresses",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Addresses_AspNetUsers_ApplicationUserId",
+                table: "Addresses",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Addresses_AspNetUsers_ApplicationUserId",
+                table: "Addresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Addresses_ApplicationUserId",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Addresses");
+        }
+    }
+}
